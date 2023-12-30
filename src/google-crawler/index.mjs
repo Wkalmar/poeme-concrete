8e28909d89264a1451bec80cc9e080900e02a2c3
Bohdan Stupak <stupakbogdan@ukr.net>
2023-12-30 15:48:32 +0200

    global try catch

--- src/google-crawler/index.mjs
+++ src/google-crawler/index.mjs
@@ -2,7 +2,13 @@ import performSearch from './google-crawler.mjs';
 
 export const handler = async () => {
     console.trace('entered index module');
-    const links = performSearch();
+    let links = [];
+    try {
+      links = performSearch();
+    }
+    catch (err) {
+      console.error(err);
+    }
 
     const response = {
       statusCode: 200,

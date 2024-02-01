212c9a3fbac73d0996112e186ca134a0223eafa8
Bohdan Stupak <stupakbogdan@ukr.net>
2024-02-01 19:29:05 +0200

    use html template

--- src/reducer/index.mjs
+++ src/reducer/index.mjs
@@ -4,5 +4,18 @@ import extractMostColorfulSentences from "./poem-selector.mjs"
 export const handler = async (data) => {
     console.trace(`entered reducer module. received ${data}`);
     const poem = extractMostColorfulSentences(data, 10);
-    console.trace(`extracted poem: ${poem}`);
+    const formatted =
+        poem
+            .map(p => `<p>${p}</p>`)
+            .join("\n");
+    const html = renderTemplate(formatted);
+    console.trace(`rendered html: ${html}`);
 }
+
+const renderTemplate = (poem) => {
+    const template = fs.readFileSync('./template.html', 'utf8');
+
+    return Mustache.render(template, {
+      poem: poem
+    });
+  }
